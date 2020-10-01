4a60e050f1fad2ca65f65d11b47690e89783767d
Hrushabh Hilwade <67234633+Ravan1803@users.noreply.github.com>
2020-10-01 13:33:26 +0530

    Update Pyhton PRint funtion.py

--- Pyhton PRint funtion.py
+++ Pyhton PRint funtion.py
@@ -1,3 +1,6 @@
-print("This is a one senteaen print")
-print("This is a multi Line print")
-name=input("Entre Your name)
+print("This is a one line sentence print")
+print('''
+this is a multiline 
+print function
+''')
+name=input("Enter Your name")
